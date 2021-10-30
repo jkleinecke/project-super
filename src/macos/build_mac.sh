36c2c23040f2b6e72e054815a0e670ed87dbe52e
James Kleinecke <jkleinecke@capsher.com>
2021-10-29 20:03:43 -0500

    MacOS platform now launches a window

--- src/macos/build_mac.sh
+++ src/macos/build_mac.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+CompilerFlags="-fno-exceptions -fno-rtti  -g -std=c++11 -Wall -Wno-format -Wno-switch -Wno-write-strings -Wno-multichar -Wno-unused-function -Wno-unused-variable -Wno-missing-braces"
+CompilerDefines="-DPROJECTSUPER_INTERNAL=1 -DPROJECTSUPER_SLOW=1 -DPROJECTSUPER_MACOS=1"
+
+LinkerFlags="-lstdc++ -framework Cocoa -framework IOKit -framework AudioUnit"
+
+echo $CompilerFlags
 mkdir build
 pushd build
-clang++ ../src/macos_platform_main.mm -o macos_platform_main
+clang++ $CompilerFlags $CompilerDefines $LinkerFlags ../src/macos/macos_platform_main.mm -o project_super 
 popd
--- src/macos/macos_platform_main.mm
+++ src/macos/macos_platform_main.mm
@@ -2,12 +2,137 @@
 #include <Cocoa/Cocoa.h>
 
 #include <stdio.h>
+#include "../ps_platform.h"
+
+global_variable bool32 GlobalRunning = true;
+
+struct MacosContext
+{
+	//HandmadeAppDelegate* AppDelegate;
+	NSWindow* Window;
+	NSString* AppName;
+	NSString* WorkingDirectory;
+};
+
+void MacosProcessMessages(MacosContext& context)
+{
+    NSEvent* Event;
+
+	//ZeroStruct(GameData->NewInput->FKeyPressed);
+	//memcpy(GameData->OldKeyboardState, GameData->KeyboardState, sizeof(GameData->KeyboardState));
+
+	do
+	{
+		Event = [NSApp nextEventMatchingMask:NSUIntegerMax
+									untilDate:nil
+										inMode:NSDefaultRunLoopMode
+									dequeue:YES];
+
+		switch ([Event type])
+		{
+/*
+			case NSKeyDown:
+			case NSKeyUp:
+			{
+				u32 KeyCode = [Event keyCode];
+				unichar C = [[Event charactersIgnoringModifiers] characterAtIndex:0];
+				u32 ModifierFlags = [Event modifierFlags];
+				int CommandKeyFlag = (ModifierFlags & NSCommandKeyMask) > 0;
+				int ControlKeyFlag = (ModifierFlags & NSControlKeyMask) > 0;
+				int AlternateKeyFlag = (ModifierFlags & NSAlternateKeyMask) > 0;
+				int ShiftKeyFlag = (ModifierFlags & NSShiftKeyMask) > 0;
+
+				int KeyDownFlag = ([Event type] == NSKeyDown) ? 1 : 0;
+				GameData->KeyboardState[KeyCode] = KeyDownFlag;
+
+				//printf("%s: keyCode: %d  unichar: %c\n", NSKeyDown ? "KeyDown" : "KeyUp", [Event keyCode], C);
+
+				OSXKeyProcessing(KeyDownFlag, KeyCode, C,
+								ShiftKeyFlag, CommandKeyFlag, ControlKeyFlag, AlternateKeyFlag,
+								GameData->NewInput, GameData);
+			} break;
+
+			case NSFlagsChanged:
+			{
+				u32 KeyCode = 0;
+				u32 ModifierFlags = [Event modifierFlags];
+				int CommandKeyFlag = (ModifierFlags & NSCommandKeyMask) > 0;
+				int ControlKeyFlag = (ModifierFlags & NSControlKeyMask) > 0;
+				int AlternateKeyFlag = (ModifierFlags & NSAlternateKeyMask) > 0;
+				int ShiftKeyFlag = (ModifierFlags & NSShiftKeyMask) > 0;
+
+				GameData->KeyboardState[kVK_Command] = CommandKeyFlag;
+				GameData->KeyboardState[kVK_Control] = ControlKeyFlag;
+				GameData->KeyboardState[kVK_Alternate] = AlternateKeyFlag;
+				GameData->KeyboardState[kVK_Shift] = ShiftKeyFlag;
+
+				int KeyDownFlag = 0;
+
+				if (CommandKeyFlag != GameData->OldKeyboardState[kVK_Command])
+				{
+					KeyCode = kVK_Command;
+
+					if (CommandKeyFlag)
+					{
+						KeyDownFlag = 1;
+					}
+				}
+
+				if (ControlKeyFlag != GameData->OldKeyboardState[kVK_Control])
+				{
+					KeyCode = kVK_Control;
+
+					if (ControlKeyFlag)
+					{
+						KeyDownFlag = 1;
+					}
+				}
+
+				if (AlternateKeyFlag != GameData->OldKeyboardState[kVK_Alternate])
+				{
+					KeyCode = kVK_Option;
+
+					if (AlternateKeyFlag)
+					{
+						KeyDownFlag = 1;
+					}
+				}
+
+				if (ShiftKeyFlag != GameData->OldKeyboardState[kVK_Shift])
+				{
+					if (ShiftKeyFlag)
+					{
+						KeyDownFlag = 1;
+					}
+
+					KeyCode = kVK_Shift;
+				}
+
+				//printf("Keyboard flags changed: Cmd: %d  Ctrl: %d  Opt: %d  Shift: %d\n",
+			//			CommandKeyFlag, ControlKeyFlag, AlternateKeyFlag, ShiftKeyFlag);
+
+				OSXKeyProcessing(-1, KeyCode, KeyCode,
+								ShiftKeyFlag, CommandKeyFlag, ControlKeyFlag, AlternateKeyFlag,
+								GameData->NewInput, GameData);
+			} break;
+*/
+			default:
+				[NSApp sendEvent:Event];
+		}
+	} while (Event != nil);
+
+}
 
 int main(int argc, const char* argv[])
 {
     @autoreleasepool
     {
+        MacosContext macContext = {};
+
         NSString* appName = @"Project Super";
+        f32 windowWidth = 1280.0f;
+        f32 windowHeight = 720.0f;
+
         // Cocoa boilerplate startup code
         NSApplication* app = [NSApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
@@ -16,26 +141,26 @@ int main(int argc, const char* argv[])
         NSString *dir = [[NSFileManager defaultManager] currentDirectoryPath];
 
         NSFileManager* FileManager = [NSFileManager defaultManager];
-        Context.WorkingDirectory = [NSString stringWithFormat:@"%@/Contents/Resources",
+        macContext.WorkingDirectory = [NSString stringWithFormat:@"%@/Contents/Resources",
             [[NSBundle mainBundle] bundlePath]];
-        if ([FileManager changeCurrentDirectoryPath:Context.WorkingDirectory] == NO)
+        if ([FileManager changeCurrentDirectoryPath:macContext.WorkingDirectory] == NO)
         {
-            Assert(0);
+            ASSERT(0);
         }
-        NSLog(@"working directory: %@", Context.WorkingDirectory);
+        NSLog(@"working directory: %@", macContext.WorkingDirectory);
 
-        Context.AppDelegate = [[HandmadeAppDelegate alloc] init];
-        [app setDelegate:Context.AppDelegate];
+        //macContext.AppDelegate = [[HandmadeAppDelegate alloc] init];
+        //[app setDelegate:Context.AppDelegate];
 
         [NSApp finishLaunching];
 
         // Create the main application window
         NSRect ScreenRect = [[NSScreen mainScreen] frame];
 
-        NSRect InitialFrame = NSMakeRect((ScreenRect.size.width - WindowWidth) * 0.5,
-                                        (ScreenRect.size.height - WindowHeight) * 0.5,
-                                        WindowWidth,
-                                        WindowHeight);
+        NSRect InitialFrame = NSMakeRect((ScreenRect.size.width - windowWidth) * 0.5,
+                                        (ScreenRect.size.height - windowHeight) * 0.5,
+                                        windowWidth,
+                                        windowHeight);
 
         NSWindow* Window = [[NSWindow alloc] initWithContentRect:InitialFrame
                                         styleMask:NSWindowStyleMaskTitled
@@ -46,7 +171,7 @@ int main(int argc, const char* argv[])
                                         defer:NO];
 
         [Window setBackgroundColor: NSColor.redColor];
-        [Window setDelegate:Context.AppDelegate];
+        //[Window setDelegate:Context.AppDelegate];
 
         NSView* CV = [Window contentView];
         [CV setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
@@ -55,6 +180,11 @@ int main(int argc, const char* argv[])
         [Window setMinSize:NSMakeSize(160, 90)];
         [Window setTitle:appName];
         [Window makeKeyAndOrderFront:nil];
+
+        while(GlobalRunning)
+        {
+            MacosProcessMessages(macContext);
+        }
     }
 
     return 0;
