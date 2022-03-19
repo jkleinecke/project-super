8bac5ffe3ee54f93169de71c179729d94bc488d3
James Kleinecke <jameskleinecke@gmail.com>
2022-03-19 09:38:22 -0500

    Lighting model (#4)
    * Initial pass at setting a lighting model in the renderer
    
    * Builds and provides setup for passing lighting info to the GPU, but there's a bug in the vulkan backend that creates the descriptor set layouts preventing it from running properly
    
    * Fixed creation of full graphics kernel program descriptor set layouts, need to fix crash in model loading code
    
    * Kinda fixed the model loading, the box looks weird though
    
    * Fixed projection issues rendering the gltf cube!
    
    * Fixed CpuToGpu buffers on MacOS
    
    * Added light to scene and setup rotating cube
    
    * Improved macos platform layer by adding AppDelegate handling to close the app
    
    * Changed test render over to spheres
    
    * Setup simple PBR shading model for a sphere in the scene
    
    * Fixed subtle PBR lighting bug.  Tried to render with many materials but doesn't quite work yet
    
    * Setup test render of 49 spheres with varying amounts of metallic and roughness material properties
    
    * Changed mesh material to load properties from textures
    
    * Fixed bug with multiple buffer descriptors, same issue as multiple image descriptors
    
    * Some cleanup and separated out a general TODO text file
    
    Co-authored-by: James Kleinecke <jkleinecke@capsher.com>

--- src/ps_game.cpp
+++ src/ps_game.cpp
@@ -3,9 +3,6 @@
 #include "ps_render.cpp"
 #include "ps_asset.cpp"
 
-
-
-#if 0
 //#define GAME_LOG(msg, ...) Platform.Log(__FILE__, __LINE__, msg, __VA_ARGS__)
 #define GAME_LOG
 
@@ -33,127 +30,18 @@ struct GameTestState
     u32 indexIntoLastSecondSamples;
 };
 
-internal void RenderVerticalLine(const GraphicsContext& graphics, u32 x, u32 y1, u32 y2, u32 color)
-{
-    // normalize so that y1 is always < than y2
-    if(y1 > y2)
-    {
-        int t = y1;
-        y1 = y2;
-        y2 = t;
-    }
-    
-    IFF(y2 >= graphics.buffer_height, y2 = graphics.buffer_height-1);
-    
-    u32* pixels = (u32*)graphics.buffer;
-    
-    for(u32 row = y1; row <= y2; ++row)
-    {
-        pixels[(row * graphics.buffer_width) + x] = color;
-    }
-}
-
-internal void RenderHorizontalLine(const GraphicsContext& graphics, u32 x1, u32 x2, u32 y, u32 color)
-{
-    // normalize so that x1 is always < x2
-    if(x1 > x2)
-    {
-        int t = x1;
-        x1 = x2;
-        x2 = t;
-    }
-    
-    IFF(x2 >= graphics.buffer_width, x2 = graphics.buffer_width-1);
-    
-    u32* pixels = (u32*)graphics.buffer;
-    for(u32 col = x1; col <= x2; ++col)
-    {
-        pixels[(y * graphics.buffer_width) + col] = color;
-    }
-}
-
 internal void
-RenderAudioWave(const GraphicsContext& graphics, GameTestState& gameState)
-{
-    ZeroArray(graphics.buffer_height * graphics.buffer_pitch, (u8*)graphics.buffer);
-    
-    // now render the audio wave
-    u32* pixels = (u32*)graphics.buffer;
-    i16* audioSamples = gameState.lastSecondAudioSamples; 
-    int samplesPerColumn = 48000 / graphics.buffer_width;
-    
-    int maxY = (int)graphics.buffer_height;
-    int midY = (int)graphics.buffer_height / 2;
-    
-    u32 leftColor = 255;        // blue
-    u32 rightColor = 255 << 16; // red
-    
-    const f32 amplitude = (f32)(TONE_AMPLITUDE * 4);
-    
-    RenderHorizontalLine(graphics, 0, graphics.buffer_width, midY, 100 << 8);
-    RenderVerticalLine(graphics, graphics.buffer_width/2, 0, graphics.buffer_height, 100 << 8);
-    
-    int lastLeftY = midY;
-    int lastRightY = midY;
-    for(u32 x = 0; x < graphics.buffer_width; ++x)
-    {
-        for(int sampleIndex = 0; sampleIndex < samplesPerColumn; ++sampleIndex)
-        {
-            f32 fLeftSampleValue = (f32)(*audioSamples) / amplitude;
-            ++audioSamples;
-            f32 fRightSampleValue = (f32)(*audioSamples) / amplitude;
-            ++audioSamples;
-            
-            IFF(fLeftSampleValue > 1.0f, fLeftSampleValue = 1.0f);
-            IFF(fLeftSampleValue < -1.0f, fLeftSampleValue = -1.0f);
-            IFF(fRightSampleValue > 1.0f, fRightSampleValue = 1.0f);
-            IFF(fRightSampleValue < -1.0f, fRightSampleValue = -1.0f);
-            
-            // now map the -1..1 sample values into the y coordinate
-            int ly = midY + (int)(fLeftSampleValue * (midY));
-            int ry = midY + (int)(fRightSampleValue * (midY));
-            
-#if 0
-            RenderVerticalLine(graphics, x, lastLeftY, ly, leftColor);
-#endif
-            RenderVerticalLine(graphics, x, lastRightY, ry, rightColor);
-            
-            lastLeftY = ly;
-            lastRightY = ry;
-        }
-    }
-}
-
-internal void
-RenderGradient(const GraphicsContext& graphics, GameTestState& gameState)
-{
-    uint8* row = (uint8*)graphics.buffer;
-    for(uint32 y = 0; y < graphics.buffer_height; ++y)
-    {
-        uint32* pixel = (uint32*)row;
-        for(uint32 x = 0; x < graphics.buffer_width; ++x)
-        {
-            uint8 green = (uint8)(y + gameState.y_offset);
-            uint8 blue = (uint8)(x + gameState.x_offset);
-            
-            *pixel++ = (green << 8) | blue;
-        }
-        
-        row += graphics.buffer_pitch;   
-    }
-}
-
-internal void
-FillSoundBuffer(AudioContext& audio, GameTestState& gameState, FrameContext& frame)
+FillSoundBuffer(AudioContext& audio, game_state& game)
 {
     const AudioContextDesc& desc = audio.descriptor;
     u32 numRequestedSamples = audio.samplesRequested;
-    void* transientMemoryPosition = frame.transientMemory.freePointer;
     // we'll accumulate the samples in floating point
-    f32* toneSamples = (f32*)PushArray(frame.transientMemory, sizeof(f32), numRequestedSamples);
+    f32* toneSamples = (f32*)PushArray(*game.frameArena, numRequestedSamples, f32);
     // clear the samples to 0
     ZeroArray(numRequestedSamples, toneSamples);
     
+    // TODO(james): Implement playing sounds / music
+#if 0
     // all sounds are treated as mono sounds for now
     // TODO(james): Handle stereo
     for(u32 sampleIndex = 0; sampleIndex < numRequestedSamples; ++sampleIndex)
@@ -164,10 +52,14 @@ FillSoundBuffer(AudioContext& audio, GameTestState& gameState, FrameContext& fra
             if(tone.isActive)
             {
                 toneSamples[sampleIndex] += tone.tone_samples[tone.playbackSampleIndex++];
-                IFF(tone.playbackSampleIndex >= tone.sample_count, tone.playbackSampleIndex = 0);
+                if(tone.playbackSampleIndex >= tone.sample_count)
+                {
+                     tone.playbackSampleIndex = 0;
+                }
             }
         }        
-    }   
+    }
+#endif   
     
     // Now we'll take the accumulated samples and convert to 16-bit and add in the volume
     
@@ -191,9 +83,6 @@ FillSoundBuffer(AudioContext& audio, GameTestState& gameState, FrameContext& fra
         *SampleOut++ = SampleValue;
         *SampleOut++ = SampleValue;
     }
-    
-    // reset our scratch memory
-    frame.transientMemory.freePointer = transientMemoryPosition;
 }
 
 internal inline
@@ -207,182 +96,6 @@ void ToggleSoundTone(SoundTone& tone, InputButton& button)
         GAME_LOG("Sound tone %d is %s", tone.toneIndex, tone.isActive ? "ACTIVE" : "INACTIVE");
     }
 }
-
-// UBOs have alignment requirements depending on the data being uploaded
-// so it is always good to be specific about the alignment for a UBO
-struct UniformBufferObject  // TODO(james): this is only temporary...
-{
-    alignas(16) m4 model;
-    alignas(16) m4 view;
-    alignas(16) m4 proj;
-};
-
-internal inline
-void RenderScene(GraphicsContext& graphics, GameTestState& gameState, FrameContext& frame)
-{
-    VkExtent2D extent = graphics.device->extent;    // TODO
-    
-    // update the g_UBO with the latest matrices
-    {
-        local_persist f32 accumlated_elapsedFrameTime = 0.0f;
-        
-        //accumlated_elapsedFrameTime += clock.elapsedFrameTime;
-        
-        u32 width = extent.width;
-        u32 height = extent.height;
-        
-        UniformBufferObject ubo{};
-        // Rotates 90 degrees a second
-        ubo.model = Rotate(accumlated_elapsedFrameTime * 90.0f, Vec3(0.0f, 0.0f, 1.0f));
-        ubo.view = LookAt(Vec3(4.0f, 4.0f, 4.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 1.0f));
-        ubo.proj = Perspective(45.0f, (f32)width, (f32)height, 0.1f, 10.0f);
-        //ubo.proj.Elements[1][1] *= -1;
-        
-        vg_buffer& uboBuffer = graphics.device->pCurFrame->camera_buffer;   // TODO
-        graphics.api->UpdateBufferData(graphics.device, uboBuffer, sizeof(ubo), &ubo);
-    }
-    
-    VkDescriptorSetLayout descriptorLayout = graphics.device->pipeline.descriptorLayout;   // TODO
-    VkDescriptorSet shaderDescriptor = graphics.api->CreateDescriptor(graphics.device, descriptorLayout);
-    
-    VkDescriptorBufferInfo bufferInfo{};
-    bufferInfo.buffer = graphics.device->pCurFrame->camera_buffer.handle;   // TODO
-    bufferInfo.offset = 0;
-    bufferInfo.range = sizeof(UniformBufferObject);
-    
-    VkDescriptorImageInfo imageInfo{};
-    imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    imageInfo.imageView = graphics.device->texture.view;  // TODO
-    imageInfo.sampler = graphics.device->sampler.handle; // TODO
-    
-    VkWriteDescriptorSet writes[] = {
-        vkInit_write_descriptor_buffer(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, shaderDescriptor, &bufferInfo, 0),
-        vkInit_write_descriptor_image(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, shaderDescriptor, &imageInfo, 1)
-    };
-    
-    graphics.api->UpdateDescriptorSets(graphics.device, ARRAY_COUNT(writes), writes);
-    
-    VkCommandBuffer cmds = graphics.api->GetCmdBuffer(graphics.device);
-    VkFramebuffer framebuffer = graphics.api->GetFramebuffer(graphics.device);
-    
-    graphics.api->BeginRecordingCmds(cmds);
-    
-    VkClearValue clearValues[2] = {};
-    clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
-    clearValues[1].depthStencil = {1.0f, 0};
-    VkRenderPass pass = graphics.device->renderPass.handle;    // TODO
-    graphics.api->BeginRenderPass(cmds, pass, ARRAY_COUNT(clearValues), clearValues, extent, framebuffer);
-    
-    VkPipeline pipeline = graphics.device->pipeline.handle; // TODO
-    graphics.api->BindPipeline(cmds, pipeline);
-    
-    VkPipelineLayout pipelineLayout = graphics.device->pipeline.layout; // TODO
-    graphics.api->BindDescriptorSets(cmds, pipelineLayout, 1, &shaderDescriptor);
-    
-    VkBuffer vertexBuffer = graphics.device->vertex_buffer.handle;  // TODO
-    VkDeviceSize offsets[] = {0};
-    graphics.api->BindVertexBuffers(cmds, 1, &vertexBuffer, offsets);
-    
-    VkBuffer indexBuffer = graphics.device->index_buffer.handle;    // TODO
-    graphics.api->BindIndexBuffer(cmds, indexBuffer);
-    
-    u32 count = 11484;  // TODO: comes from model
-    graphics.api->DrawIndexed(cmds, count, 1);
-    
-    graphics.api->EndRenderPass(cmds);
-    graphics.api->EndRecordingCmds(cmds);
-}
-
-#endif
-
-#if 0
-internal void
-RenderModel(render_commands& cmds, model_asset* model, u32 materialId, m4& viewProj, m4& world, m4& scale)
-{
-    m4 worldNormal = world;
-    m4 scaled = world * scale;
-    m4 mvp = viewProj * scaled;
-
-    render_geometry mesh;
-    mesh.indexCount = model->indexCount;
-    mesh.indexBuffer = model->index_id;
-    mesh.vertexBuffer = model->vertex_id;
-
-    // render_material_binding bindings[1];
-    // bindings[0].type                = RenderMaterialBindingType::Image;
-    // bindings[0].layoutIndex         = 0;        // TODO(james): perhaps change this to a "named" layout?
-    // bindings[0].bindingIndex        = 0;
-    // bindings[0].image_id            = model->texture_id;
-    // bindings[0].image_sampler_index = 0;    // TODO(james): This needs to be easier to figure out that just "knowing" which index to use
-
-    // u32 bindingCount = 0;;
-
-    // if(model->texture_id)
-    // {
-    //     bindingCount = ARRAY_COUNT(bindings);
-    // }
-
-    PushCmd_DrawObject(cmds, mesh, scaled, scaled, worldNormal, materialId);
-}
-
-internal
-void BuildRenderCommands(game_state& state, render_context& render, const GameClock& clock)
-{
-    render_commands& cmds = render.commands;
-
-    BeginRenderInfo renderInfo{};
-    renderInfo.viewportPosition = Vec2i(0,0);
-    renderInfo.viewportSize = render.renderDimensions;
-    renderInfo.time = clock.totalTime;
-    renderInfo.timeDelta = clock.elapsedFrameTime;
-    renderInfo.cameraPos = state.camera.position;
-    renderInfo.cameraView = LookAt(state.camera.position, state.camera.target, V3_Y_UP);
-    renderInfo.cameraProj = state.cameraProjection;
-    BeginRenderCommands(cmds, renderInfo);
-    
-    m4 viewProj = state.cameraProjection * renderInfo.cameraView;
-
-    // {
-    //     m4 mvp = viewProj * M4_IDENTITY;
-    //     RenderModel(cmds, state.assets->vikingModel, mvp);
-    // }
-
-    // {
-    //     v3 scale = Vec3(state.skullScaleFactor,state.skullScaleFactor,state.skullScaleFactor);
-    //     // for each object need to compute the mvp
-    //     m4 model = Translate(state.skullPosition) * Rotate(state.skullRotationAngle, Vec3i(0,0,1)) * Scale(scale);//M4_IDENTITY;
-    //     m4 mvp = viewProj * model;
-
-    //     RenderModel(cmds, state.assets->skullModel, mvp);
-    // }
-
-    // v3 lite_ambient = Vec3(0.2f, 0.2f, 0.2f);
-    v3 lite_ambient = Vec3(0.1f, 0.1f, 0.1f);
-    // v3 lite_diffuse = Vec3(0.5f, 0.5f, 0.5f);
-    v3 lite_diffuse = Vec3(0.3f, 0.3f, 0.3f);
-    v3 lite_spec = Vec3(1.0f, 1.0f, 1.0f);
-
-    PushCmd_UpdateLight(cmds, state.lightPosition, lite_ambient, lite_diffuse, lite_spec);
-
-    // PushCmd_UsePipeline(cmds, state.assets->mapPipelines->get(pl_box)->id);;
-
-    // {
-    //     m4 scale = Scale(Vec3(state.scaleFactor,state.scaleFactor,state.scaleFactor));
-    //     m4 model = Translate(state.position) * Rotate(state.rotationAngle, Vec3i(0,1,0)) ;
-    //     RenderModel(cmds, state.assets->models, state.assets->mapMaterials->get(mat_box)->id, viewProj, model, scale);
-    // }
-
-    // PushCmd_UsePipeline(cmds, state.assets->mapPipelines->get(pl_lightbox)->id);
-
-    // {
-    //     m4 scale = Scale(Vec3(state.lightScale, state.lightScale, state.lightScale));
-    //     m4 model = Translate(state.lightPosition);
-    //     RenderModel(cmds, state.assets->models, state.assets->mapMaterials->get(mat_box)->id, viewProj, model, scale);
-    // }
-
-    EndRenderCommands(cmds);
-}
-#endif
 
 platform_api Platform;
 extern "C"
@@ -528,159 +241,8 @@ GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
         }
     }
 
-    // const f32 lightSpeedPerSecond = 1 * input.clock.elapsedFrameTime;
-    // local_persist b32 lightDirUp = true;
-    // local_persist b32 lightDirRight = true;
-    // if(lightDirUp)
-    // {
-    //     gameState.lightPosition.Y += lightSpeedPerSecond;
-    //     if(gameState.lightPosition.Y >= 5.0f)
-    //     {
-    //         lightDirUp = false;
-    //     }
-    // }
-    // else
-    // {
-    //     gameState.lightPosition.Y -= lightSpeedPerSecond;
-    //     if(gameState.lightPosition.Y <= -5.0f)
-    //     {
-    //         lightDirUp = true;
-    //     }
-    // }
-
     gameState.camera.target = gameState.position;
+    
+    FillSoundBuffer(audio, gameState);    
     RenderFrame(*gameState.renderer, gameState, input.clock);
-    
-    // BuildRenderCommands(gameState, render, input.clock);
-    
-#if 0
-    // TODO(james): needs a better allocation scheme 
-    GameTestState& gameState = *(GameTestState*)frame.persistantMemory.basePointer;
-    if(frame.persistantMemory.basePointer == frame.persistantMemory.freePointer)
-    {
-        PushStruct(frame.persistantMemory, sizeof(gameState));
-        // memory isn't initialized
-        gameState.x_offset = 0;
-        gameState.y_offset = 0;
-        
-        // 6 (E)	329.63 Hz	E4
-        // 5 (B)	246.94 Hz	B4
-        // 4 (G)	196.00 Hz	G3
-        // 3 (D)	146.83 Hz	D3
-        // 2 (A)	110.00 Hz	A3
-        // 1 (E)	82.41 Hz	E2
-        
-        f32 frequencies[] = {
-            CalcFrequency(2,7),
-            CalcFrequency(3,0),
-            CalcFrequency(3,5),
-            CalcFrequency(3,10),
-            CalcFrequency(4,2),
-            CalcFrequency(4,7)
-        };
-        
-        const AudioContextDesc& desc = audio.descriptor;
-        
-        for(int toneIndex = 0; toneIndex < 6; ++toneIndex)
-        {
-            SoundTone& tone = gameState.tones[toneIndex];
-            tone.toneIndex = toneIndex;
-            tone.isActive = false;
-            tone.playbackSampleIndex = 0;
-            
-            f32 freq = frequencies[toneIndex];
-            // now fill out the samples
-            u32 samplesPerPeriod = (u32)(((f32)desc.samplesPerSecond/freq) + 0.5f);
-            tone.sample_count = samplesPerPeriod;
-            tone.tone_samples = (nf32*)PushArray(frame.persistantMemory, sizeof(nf32), tone.sample_count);
-            
-            f32 phase = 0.0f;
-            nf32 lastValue = 0.0f;
-            nf32* toneSamples = tone.tone_samples;
-            for(u32 sampleIndex = 0; sampleIndex < tone.sample_count; ++sampleIndex)
-            {
-                lastValue = Oscilator(phase, freq, (f32)desc.samplesPerSecond, lastValue);
-                toneSamples[sampleIndex] = lastValue;
-            }
-        }
-        
-        ZeroArray(ARRAY_COUNT(gameState.lastSecondAudioSamples), gameState.lastSecondAudioSamples);
-        gameState.indexIntoLastSecondSamples = 0;
-    }
-    
-    for(int controllerIndex = 0; controllerIndex < 5; ++controllerIndex)
-    {
-        InputController& controller = input.controllers[controllerIndex];
-        
-        if(controller.isConnected)
-        {
-            if(controller.isAnalog)
-            {
-                gameState.x_offset += (int)(controller.leftStick.x * 5.0f);
-                gameState.y_offset += (int)(controller.leftStick.y * 5.0f);
-                
-                // frequency range of 80Hz -> 1200Hz
-                // const real32 min = 250.0f;
-                // const real32 max = 278.0f;
-                // const real32 halfMagnitude = (max - min)/2.0f;
-                // gameState.toneHz = min + halfMagnitude + controller.rightStick.y * halfMagnitude;
-            }
-            
-            if(controller.left.pressed)
-            {
-                gameState.x_offset -= 10;
-            }
-            if(controller.right.pressed)
-            {
-                gameState.x_offset += 10;
-            }
-            if(controller.up.pressed)
-            {
-                gameState.y_offset += 10;
-            }
-            if(controller.down.pressed)
-            {
-                gameState.y_offset -= 10;
-            }
-            
-            
-            for(int toneIndex = 0; toneIndex < ARRAY_COUNT(gameState.tones); ++toneIndex)
-            {
-                ToggleSoundTone(gameState.tones[toneIndex], controller.buttons[toneIndex]);
-            }
-            
-            controller.leftFeedbackMotor = controller.leftTrigger.value;
-            controller.rightFeedbackMotor = controller.rightTrigger.value;
-        }
-    }
-    
-    FillSoundBuffer(audio, gameState, frame);
-    RenderScene(graphics, gameState, frame);
-    
-    now keep a ring buffer copy of the frames audio samples
-        u32 sampleCountToCopy = audio.samplesWritten * 2;
-    u32 nextIndex = gameState.indexIntoLastSecondSamples + sampleCountToCopy; 
-    i16* audioBuffer = (i16*)audio.streamBuffer.data;
-    const AudioContextDesc& desc = audio.descriptor;
-    
-    const u32 maxIndex = (desc.samplesPerSecond * desc.numChannels) - 1;
-    
-    if(nextIndex > maxIndex)
-    {
-        u32 entriesToEnd = (maxIndex - gameState.indexIntoLastSecondSamples);
-        // have to do 2 copies
-        CopyArray(entriesToEnd, audioBuffer, &gameState.lastSecondAudioSamples[gameState.indexIntoLastSecondSamples]);
-        u32 secondIndex = sampleCountToCopy - entriesToEnd;
-        CopyArray(secondIndex, &audioBuffer[entriesToEnd], gameState.lastSecondAudioSamples);
-        nextIndex = secondIndex;
-    }
-    else
-    {
-        CopyArray(sampleCountToCopy, audioBuffer, &gameState.lastSecondAudioSamples[gameState.indexIntoLastSecondSamples]);
-    }
-    gameState.indexIntoLastSecondSamples = nextIndex;
-    
-    //RenderGradient(graphics, gameState); 
-    RenderAudioWave(graphics, gameState);
-#endif
 }
